34768ff4545aeb58f8453b05f110a29ae2c3a836
Goma <guilherme.lobo.gomes@tecnico.ulisboa.pt>
2023-12-14 03:04:21 +0000

    mutex_in not working

--- p1_base/parser.c
+++ p1_base/parser.c
@@ -4,8 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #include "constants.h"
+
+static pthread_mutex_t mutex_in = PTHREAD_MUTEX_INITIALIZER;
 
 static int read_uint(int fd, unsigned int *value, char *next) {
   char buf[16];
@@ -46,7 +49,9 @@ static void cleanup(int fd) {
 
 enum Command get_next(int fd) {
   char buf[16];
+  pthread_mutex_lock(&mutex_in);
   if (read(fd, buf, 1) != 1) {
+    pthread_mutex_unlock(&mutex_in);
     return EOC;
   }
 
@@ -54,14 +59,15 @@ enum Command get_next(int fd) {
     case 'C':
       if (read(fd, buf + 1, 6) != 6 || strncmp(buf, "CREATE ", 7) != 0) {
         cleanup(fd);
+        pthread_mutex_unlock(&mutex_in);
         return CMD_INVALID;
       }
-
       return CMD_CREATE;
 
     case 'R':
       if (read(fd, buf + 1, 7) != 7 || strncmp(buf, "RESERVE ", 8) != 0) {
         cleanup(fd);
+        pthread_mutex_unlock(&mutex_in);
         return CMD_INVALID;
       }
 
@@ -70,6 +76,7 @@ enum Command get_next(int fd) {
     case 'S':
       if (read(fd, buf + 1, 4) != 4 || strncmp(buf, "SHOW ", 5) != 0) {
         cleanup(fd);
+        pthread_mutex_unlock(&mutex_in);
         return CMD_INVALID;
       }
 
@@ -78,74 +85,85 @@ enum Command get_next(int fd) {
     case 'L':
       if (read(fd, buf + 1, 3) != 3 || strncmp(buf, "LIST", 4) != 0) {
         cleanup(fd);
+        pthread_mutex_unlock(&mutex_in);
         return CMD_INVALID;
       }
 
       if (read(fd, buf + 4, 1) != 0 && buf[4] != '\n') {
         cleanup(fd);
+        pthread_mutex_unlock(&mutex_in);
         return CMD_INVALID;
       }
-
+      pthread_mutex_unlock(&mutex_in);
       return CMD_LIST_EVENTS;
 
     case 'B':
       if (read(fd, buf + 1, 6) != 6 || strncmp(buf, "BARRIER", 7) != 0) {
         cleanup(fd);
+        pthread_mutex_unlock(&mutex_in);
         return CMD_INVALID;
       }
 
       if (read(fd, buf + 7, 1) != 0 && buf[7] != '\n') {
         cleanup(fd);
+        pthread_mutex_unlock(&mutex_in);
         return CMD_INVALID;
       }
-
+      pthread_mutex_unlock(&mutex_in);
       return CMD_BARRIER;
 
     case 'W':
       if (read(fd, buf + 1, 4) != 4 || strncmp(buf, "WAIT ", 5) != 0) {
         cleanup(fd);
+        pthread_mutex_unlock(&mutex_in);
         return CMD_INVALID;
       }
-
       return CMD_WAIT;
 
     case 'H':
       if (read(fd, buf + 1, 3) != 3 || strncmp(buf, "HELP", 4) != 0) {
         cleanup(fd);
+        pthread_mutex_unlock(&mutex_in);
         return CMD_INVALID;
       }
 
       if (read(fd, buf + 4, 1) != 0 && buf[4] != '\n') {
         cleanup(fd);
+        pthread_mutex_unlock(&mutex_in);
         return CMD_INVALID;
       }
-
+      pthread_mutex_unlock(&mutex_in);
       return CMD_HELP;
 
     case '#':
       cleanup(fd);
+      pthread_mutex_unlock(&mutex_in);
       return CMD_EMPTY;
 
     case '\n':
+    pthread_mutex_unlock(&mutex_in);
       return CMD_EMPTY;
 
     default:
       cleanup(fd);
+      pthread_mutex_unlock(&mutex_in);
       return CMD_INVALID;
   }
 }
 
 int parse_create(int fd, unsigned int *event_id, size_t *num_rows, size_t *num_cols) {
   char ch;
-
+  pthread_mutex_lock(&mutex_in);
   if (read_uint(fd, event_id, &ch) != 0 || ch != ' ') {
     cleanup(fd);
+    pthread_mutex_unlock(&mutex_in);
     return 1;
   }
 
   unsigned int u_num_rows;
   if (read_uint(fd, &u_num_rows, &ch) != 0 || ch != ' ') {
     cleanup(fd);
+    pthread_mutex_unlock(&mutex_in);
     return 1;
   }
   *num_rows = (size_t)u_num_rows;
@@ -153,8 +171,10 @@ int parse_create(int fd, unsigned int *event_id, size_t *num_rows, size_t *num_c
   unsigned int u_num_cols;
   if (read_uint(fd, &u_num_cols, &ch) != 0 || (ch != '\n' && ch != '\0')) {
     cleanup(fd);
+    pthread_mutex_unlock(&mutex_in);
     return 1;
   }
+  pthread_mutex_unlock(&mutex_in);
   *num_cols = (size_t)u_num_cols;
 
   return 0;
@@ -162,14 +182,16 @@ int parse_create(int fd, unsigned int *event_id, size_t *num_rows, size_t *num_c
 
 size_t parse_reserve(int fd, size_t max, unsigned int *event_id, size_t *xs, size_t *ys) {
   char ch;
-
+  pthread_mutex_lock(&mutex_in);
   if (read_uint(fd, event_id, &ch) != 0 || ch != ' ') {
     cleanup(fd);
+    pthread_mutex_unlock(&mutex_in);
     return 0;
   }
 
   if (read(fd, &ch, 1) != 1 || ch != '[') {
     cleanup(fd);
+    pthread_mutex_unlock(&mutex_in);
     return 0;
   }
 
@@ -177,12 +199,14 @@ size_t parse_reserve(int fd, size_t max, unsigned int *event_id, size_t *xs, siz
   while (num_coords < max) {
     if (read(fd, &ch, 1) != 1 || ch != '(') {
       cleanup(fd);
+      pthread_mutex_unlock(&mutex_in);
       return 0;
     }
 
     unsigned int x;
     if (read_uint(fd, &x, &ch) != 0 || ch != ',') {
       cleanup(fd);
+      pthread_mutex_unlock(&mutex_in);
       return 0;
     }
     xs[num_coords] = (size_t)x;
@@ -190,6 +214,7 @@ size_t parse_reserve(int fd, size_t max, unsigned int *event_id, size_t *xs, siz
     unsigned int y;
     if (read_uint(fd, &y, &ch) != 0 || ch != ')') {
       cleanup(fd);
+      pthread_mutex_unlock(&mutex_in);
       return 0;
     }
     ys[num_coords] = (size_t)y;
@@ -198,6 +223,7 @@ size_t parse_reserve(int fd, size_t max, unsigned int *event_id, size_t *xs, siz
 
     if (read(fd, &ch, 1) != 1 || (ch != ' ' && ch != ']')) {
       cleanup(fd);
+      pthread_mutex_unlock(&mutex_in);
       return 0;
     }
 
@@ -208,52 +234,60 @@ size_t parse_reserve(int fd, size_t max, unsigned int *event_id, size_t *xs, siz
 
   if (num_coords == max) {
     cleanup(fd);
+    pthread_mutex_unlock(&mutex_in);
     return 0;
   }
 
   if (read(fd, &ch, 1) != 1 || (ch != '\n' && ch != '\0')) {
     cleanup(fd);
+    pthread_mutex_unlock(&mutex_in);
     return 0;
   }
-
+  pthread_mutex_unlock(&mutex_in);
   return num_coords;
 }
 
 int parse_show(int fd, unsigned int *event_id) {
   char ch;
-
+  pthread_mutex_lock(&mutex_in);
   if (read_uint(fd, event_id, &ch) != 0 || (ch != '\n' && ch != '\0')) {
     cleanup(fd);
+    pthread_mutex_unlock(&mutex_in);
     return 1;
   }
-
+  pthread_mutex_unlock(&mutex_in);
   return 0;
 }
 
 int parse_wait(int fd, unsigned int *delay, unsigned int *thread_id) {
   char ch;
-
+  pthread_mutex_lock(&mutex_in);
   if (read_uint(fd, delay, &ch) != 0) {
     cleanup(fd);
+    pthread_mutex_unlock(&mutex_in);
     return -1;
   }
 
   if (ch == ' ') {
     if (thread_id == NULL) {
       cleanup(fd);
+      pthread_mutex_unlock(&mutex_in);
       return 0;
     }
 
     if (read_uint(fd, thread_id, &ch) != 0 || (ch != '\n' && ch != '\0')) {
       cleanup(fd);
+      pthread_mutex_unlock(&mutex_in);
       return -1;
     }
-
+    pthread_mutex_unlock(&mutex_in);
     return 1;
   } else if (ch == '\n' || ch == '\0') {
+    pthread_mutex_unlock(&mutex_in);
     return 0;
   } else {
     cleanup(fd);
+    pthread_mutex_unlock(&mutex_in);
     return -1;
   }
 }
