b400a69bd82ae19343064cd7dde9b615ee6859f3
Goma <guilherme.lobo.gomes@tecnico.ulisboa.pt>
2023-12-30 19:39:24 +0000

    p-c 'buffer'

--- Part_2/proj_23-24-p2_base/server/main.c
+++ Part_2/proj_23-24-p2_base/server/main.c
@@ -13,9 +13,15 @@
 #include "common/io.h"
 #include "operations.h"
 
+
+typedef struct request {
+  int fd_req, fd_resp;
+  struct request *next;
+} request;
+
 typedef struct threadArgs {
   int *session_id_status;
-  int session_id, fd_req, fd_resp;
+  request **producer_consumer;
 } threadArgs;
 
 
@@ -30,154 +36,161 @@ int findNextAvailableSessionID (int *session_id_status) {
 void *threadFunction(void *args) {
   threadArgs *thread_args = (threadArgs *)args;
   int *session_id_status = thread_args->session_id_status;
-  int session_id = thread_args->session_id;
-  int fd_req = thread_args->fd_req;
-  int fd_resp = thread_args->fd_resp;
+  request **producer_consumer = thread_args->producer_consumer;
+  int fd_req, fd_resp, session_id;
   char buffer[BUFFER_SIZE];  
+  memset(buffer, '\0', sizeof(buffer));
   char op_code_str[2];
   char id_str[2];
-  sprintf(id_str, "%d", session_id);
-  send_msg(fd_resp, id_str);
-  memset(buffer, '\0', sizeof(buffer));
-  read_msg(fd_req, buffer, BUFFER_SIZE);
-  while (1) {
-    int ret, fd_aux;
-    size_t num_seats = 0;
-    unsigned int event_id = 0;
-    size_t xs[num_seats];
-    size_t ys[num_seats];
-    size_t rows;
-    size_t cols;
-    const char *aux_file = "aux_file.txt";
-    switch (buffer[0]) {
-      case '2':
-        session_id_status[session_id] = 0;
-        pthread_exit(NULL);
-      case '3':
-        size_t num_rows = 0;
-        size_t num_col = 0;
-        char ret_str[2];
-        sscanf(buffer, "%c %u %ld %ld", op_code_str, &event_id, &num_rows, &num_col);
-        rows = num_rows;
-        cols = num_col;
-        ret = ems_create(event_id, num_rows, num_col);
-        snprintf(ret_str, sizeof(ret), "%d", ret);
-        send_msg(fd_resp, ret_str);
-        break;
-      case '4':
-        int elements_already_read = 0;
-        int i = 0;
-        const char *ptr = buffer;
-        sscanf(buffer, "%c %u %ld%n", op_code_str, &event_id, &num_seats, &elements_already_read);
-        ptr += elements_already_read;
-        while (sscanf(ptr, "%zu", &xs[i]) == 1) {
-          ptr = strchr(ptr, ' ');
-          if(ptr == NULL)
-            break;
-          ptr++;
-          sscanf(ptr, "%zu", &ys[i]);
-          ptr = strchr(ptr, ' ');
-          if(ptr == NULL)
-            break;
-          ptr++;
-          i++;
-        }
-        ret = ems_reserve(event_id, num_seats, xs, ys);
-        snprintf(ret_str, sizeof(ret), "%d", ret);
-        send_msg(fd_resp, ret_str);
-        break;
-      case '5':
-        size_t num_elements = 2 * rows * cols + 100;
-        char *msg = (char *)malloc(num_elements);
-        if (msg == NULL) {
+  while(1){
+    while(*producer_consumer == NULL || (session_id = findNextAvailableSessionID(session_id_status)) == -1)
+      continue;
+    fd_req = (*producer_consumer)->fd_req;
+    fd_resp = (*producer_consumer)->fd_resp;
+    session_id_status[session_id] = 1;
+    sprintf(id_str, "%d", session_id);
+    send_msg(fd_resp, id_str);
+    while (1) {
+      int ret, fd_aux;
+      size_t num_seats = 0;
+      unsigned int event_id = 0;
+      size_t xs[num_seats];
+      size_t ys[num_seats];
+      size_t rows;
+      size_t cols;
+      const char *aux_file = "aux_file.txt"; //FIXME ***isto precisa de ser alterado -> cada thread tem de ter o seu ficheiro auxiliar***
+      switch (buffer[0]) {
+        case '2':
+          session_id_status[session_id] = 0;
+          break;
+        case '3':
+          size_t num_rows = 0;
+          size_t num_col = 0;
+          char ret_str[2];
+          sscanf(buffer, "%c %u %ld %ld", op_code_str, &event_id, &num_rows, &num_col);
+          rows = num_rows;
+          cols = num_col;
+          ret = ems_create(event_id, num_rows, num_col);
+          snprintf(ret_str, sizeof(ret), "%d", ret);
+          send_msg(fd_resp, ret_str);
+          break;
+        case '4':
+          int elements_already_read = 0;
+          int i = 0;
+          const char *ptr = buffer;
+          sscanf(buffer, "%c %u %ld%n", op_code_str, &event_id, &num_seats, &elements_already_read);
+          ptr += elements_already_read;
+          while (sscanf(ptr, "%zu", &xs[i]) == 1) {
+            ptr = strchr(ptr, ' ');
+            if(ptr == NULL)
+              break;
+            ptr++;
+            sscanf(ptr, "%zu", &ys[i]);
+            ptr = strchr(ptr, ' ');
+            if(ptr == NULL)
+              break;
+            ptr++;
+            i++;
+          }
+          ret = ems_reserve(event_id, num_seats, xs, ys);
+          snprintf(ret_str, sizeof(ret), "%d", ret);
+          send_msg(fd_resp, ret_str);
+          break;
+        case '5':
+          size_t num_elements = 2 * rows * cols + 100;
+          char *msg = (char *)malloc(num_elements);
+          if (msg == NULL) {
             fprintf(stderr, "Failed to allocate memory for msg\n");
             exit(1);
-        }
-        const char *msg_ptr = msg;
-        memset(msg, 0, num_elements);
-        sscanf(buffer, "%c %u", op_code_str, &event_id);
-        fd_aux = open(aux_file, O_RDWR | O_CREAT | O_TRUNC, 0644);
-        if (fd_aux < 0) {
-          fprintf(stderr, "Failed open aux file\n");
-          free(msg);
-          close(fd_aux);
-          unlink(aux_file);
-          exit(1);
-        }
-        ret = ems_show(fd_aux, event_id);
-        if(ret == 1) {
-          send_msg(fd_resp, "1");
-          close(fd_aux);
-          unlink(aux_file);
-          free(msg);
-          break;
-        }
-        sprintf(msg, "%d %zu %zu\n", ret, rows, cols);
-        msg_ptr += strlen(msg);
-        lseek(fd_aux, 0, SEEK_SET);
-        read(fd_aux, msg + strlen(msg), num_elements); 
-        send_msg(fd_resp, msg);
-        close(fd_aux);
-        unlink(aux_file);
-        free(msg);
-        break;
-      case '6':
-        fd_aux = open(aux_file, O_RDWR | O_CREAT | O_TRUNC, 0644);
-        if (fd_aux < 0) {
-          fprintf(stderr, "Failed open aux file\n");
-          exit(1);
-        }
-
-        ret = ems_list_events(fd_aux);
-        if(ret == 1) {
-          send_msg(fd_resp, "1");
-          close(fd_aux);
-          unlink(aux_file);
-          break;
-        }
-        unsigned int *ids = NULL;
-        char aux_buffer[BUFFER_SIZE];
-        size_t num_events = 0;
-        const char *aux_ptr = aux_buffer;
-
-        lseek(fd_aux, 0, SEEK_SET);
-        memset(aux_buffer, '\0', sizeof(aux_buffer));
-        read_msg(fd_aux, aux_buffer, BUFFER_SIZE);
-        
-        while(1) {
-          if (sscanf(aux_ptr, "Event: %d", &event_id) == 1) {
-            num_events++;
-            ids = realloc(ids, num_events * sizeof(unsigned int));
-            if (ids == NULL) {
-              fprintf(stderr, "Failed to realloc ids array\n");
-              close(fd_aux);
-              unlink(aux_file);
-              free(ids);
-              exit(1);
-            }
-            ids[num_events - 1] = event_id;
           }
-          aux_ptr = strchr(aux_ptr, '\n');
-          if (aux_ptr == NULL)
+          const char *msg_ptr = msg;
+          memset(msg, 0, num_elements);
+          sscanf(buffer, "%c %u", op_code_str, &event_id);
+          fd_aux = open(aux_file, O_RDWR | O_CREAT | O_TRUNC, 0644);
+          if (fd_aux < 0) {
+          fprintf(stderr, "Failed open aux file\n");
+          free(msg);
+          close(fd_aux);
+          unlink(aux_file);
+          exit(1);
+          }
+          ret = ems_show(fd_aux, event_id);
+          if(ret == 1) {
+            send_msg(fd_resp, "1");
+            close(fd_aux);
+            unlink(aux_file);
+            free(msg);
             break;
-          aux_ptr++;
-        }
+          }
+          sprintf(msg, "%d %zu %zu\n", ret, rows, cols);
+          msg_ptr += strlen(msg);
+          lseek(fd_aux, 0, SEEK_SET);
+          read(fd_aux, msg + strlen(msg), num_elements); 
+          send_msg(fd_resp, msg);
+          close(fd_aux);
+          unlink(aux_file);
+          free(msg);
+          break;
+        case '6':
+          fd_aux = open(aux_file, O_RDWR | O_CREAT | O_TRUNC, 0644);
+          if (fd_aux < 0) {
+            fprintf(stderr, "Failed open aux file\n");
+            exit(1);
+          }
+
+          ret = ems_list_events(fd_aux);
+          if(ret == 1) {
+            send_msg(fd_resp, "1");
+            close(fd_aux);
+            unlink(aux_file);
+            break;
+          }
+          unsigned int *ids = NULL;
+          char aux_buffer[BUFFER_SIZE];
+          size_t num_events = 0;
+          const char *aux_ptr = aux_buffer;
+
+          lseek(fd_aux, 0, SEEK_SET);
+          memset(aux_buffer, '\0', sizeof(aux_buffer));
+          read_msg(fd_aux, aux_buffer, BUFFER_SIZE);
         
-        char *list_events_msg = (char *)malloc(sizeof(ret) + sizeof(num_events) + (num_events * sizeof(unsigned int)) + 2);
-        sprintf(list_events_msg, "%d %zu", ret, num_events);
-        for (size_t j = 0; j < num_events; ++j) 
-          sprintf(list_events_msg + strlen(list_events_msg), " %u", ids[j]);
-        send_msg(fd_resp, list_events_msg);
-        close(fd_aux);
-        unlink(aux_file);
-        free(ids);
-        free(list_events_msg);
+          while(1) {
+            if (sscanf(aux_ptr, "Event: %d", &event_id) == 1) {
+              num_events++;
+              ids = realloc(ids, num_events * sizeof(unsigned int));
+              if (ids == NULL) {
+                fprintf(stderr, "Failed to realloc ids array\n");
+                close(fd_aux);
+                unlink(aux_file);
+                free(ids);
+                exit(1);
+              }
+              ids[num_events - 1] = event_id;
+            }
+            aux_ptr = strchr(aux_ptr, '\n');
+            if (aux_ptr == NULL)
+              break;
+            aux_ptr++;
+          }
+        
+          char *list_events_msg = (char *)malloc(sizeof(ret) + sizeof(num_events) + (num_events * sizeof(unsigned int)) + 2);
+          sprintf(list_events_msg, "%d %zu", ret, num_events);
+          for (size_t j = 0; j < num_events; ++j) 
+            sprintf(list_events_msg + strlen(list_events_msg), " %u", ids[j]);
+          send_msg(fd_resp, list_events_msg);
+          close(fd_aux);
+          unlink(aux_file);
+          free(ids);
+          free(list_events_msg);
+          break;
+      }
+      //TODO: Read from pipe
+      if (session_id_status[session_id] == 0)
         break;
+      memset(buffer, '\0', sizeof(buffer));
+      read_msg(fd_req, buffer, BUFFER_SIZE);
+      //TODO: Write new client to the producer_consumer buffer
     }
-    //TODO: Read from pipe
-    memset(buffer, '\0', sizeof(buffer));
-    read_msg(fd_req, buffer, BUFFER_SIZE);
-    //TODO: Write new client to the producer-consumer buffer
   }
 }
 
@@ -208,6 +221,7 @@ int main(int argc, char* argv[]) {
 
   //TODO: Intialize server, create worker threads
   char *pipeServer = argv[1];
+  request *producer_consumer = NULL;
   int fd_serv;
   int session_id_status[MAX_SESSION_COUNT] = {0};
   pthread_t threads[MAX_SESSION_COUNT];
@@ -236,8 +250,6 @@ int main(int argc, char* argv[]) {
     memset(buffer, '\0', sizeof(buffer));
     read_msg(fd_serv, buffer, BUFFER_SIZE);
     if (buffer[0] == '1') {
-      while((id = findNextAvailableSessionID(session_id_status)) == -1)
-        continue;
       sscanf(buffer, "%c %s %s", op_code_str, req_pipe, resp_pipe);
       if((fd_req = open(req_pipe, O_RDONLY)) < 0) {
         fprintf(stderr, "Failed to open sender named pipe\n");
@@ -248,11 +260,23 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "Failed to open receiver named pipe\n");
         return 1;
       }
-      args[id].session_id_status = session_id_status;
-      args[id].session_id = id;
-      args[id].fd_req = fd_req;
-      args[id].fd_resp = fd_resp;
-      pthread_create(&threads[id], NULL, threadFunction, (void *)&args[id]);
+      request *new_request = (request *)malloc(sizeof(request));
+      if (new_request == NULL) {
+        fprintf(stderr, "Failed to allocate memory for new_request\n");
+        return 1;
+      }
+      new_request->fd_req = fd_req;
+      new_request->fd_resp = fd_resp;
+      new_request->next = NULL;
+      if (producer_consumer == NULL) {
+        producer_consumer = new_request;
+      } else {
+        request *aux = producer_consumer;
+        while (aux->next != NULL) {
+          aux = aux->next;
+        }
+        aux->next = new_request;
+      }
     }
   }
  
